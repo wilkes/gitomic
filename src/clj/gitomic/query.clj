9debee2245a41e31c0addf727a98a8ecb0590fdc
wilkes <wilkesjoiner@gmail.com>
2015-08-29 19:00:19 -0500

    add file-commit-pair, update churn

--- src/clj/gitomic/query.clj
+++ src/clj/gitomic/query.clj
@@ -22,19 +22,15 @@
        :where [?c :commit/sha ?sha]]
      db sha))
 
-(defn file-by-path [db p]
-  (q '[:find ?f .
-       :in $ ?p
-       :where [?f :file/path ?p]]
-     db p))
-
-(defn commits-by-file-path [db r path]
+(defn commits-by-path [db r path]
   (q '[:find [?c ...]
        :in $ ?r ?p
        :where
        [?f :file/path ?p]
-       [?c :commit/files ?f]
-       [?c :commit/merge? false]]
+       [?ch :change/path ?f]
+       [?c :commit/changes ?ch]
+       [?c :commit/merge? false]
+       [?r :repo/commits ?c]]
      db r path))
 
 (defn files-in-repo [db repo]
@@ -42,24 +38,46 @@
        :in $ ?r
        :where
        [?r :repo/commits ?c]
-       [?c :commit/files ?f]]
+       [?c :commit/changes ?ch]
+       [?ch :change/path ?f]]
      db repo))
 
-(defn churn-on-path [db path]
-  (q '[:find [?c ...]
-       :in $ ?p
-       :where
-       [?f :file/path ?p]
-       [?c :commit/files ?f]
-       [?c :commit/merge? false]]
-     db path))
+(defn weight [max n]
+  (float (/ n max)))
 
 (defn churn [db r]
-  (q '[:find ?n (count ?c)
-       :in $ ?r
-       :where
-       [?r :repo/commits ?c]
-       [?c :commit/files ?f]
-       [?c :commit/merge? false]
-       [?f :file/path ?n]]
-     db r))
+  (let [pairs (reverse
+                (sort-by second
+                         (q '[:find ?p (count ?c)
+                              :in $ ?r
+                              :where
+                              [?r :repo/commits ?c]
+                              [?c :commit/merge? false]
+                              [?c :commit/changes ?ch]
+                              [?ch :change/path ?f]
+                              [?f :file/path ?p]
+                              ;[(.endsWith ?p ".rb")]
+                              ]
+                            db r)))
+        m (-> pairs first second)]
+    (mapv (fn [[p n]]
+            [p n (weight m n)])
+          pairs)))
+
+(defn file-commit-pair [db r file-path]
+  (reverse
+    (sort-by second
+                     (q '[:find ?p2 (count ?c)
+                                 :in $ ?r ?p
+                                 :where
+                                 [?f1 :file/path ?p]
+                                 [?ch1 :change/path ?f1]x
+                                 [?c :commit/changes ?ch1]
+                                 [?r :repo/commits ?c]
+                                 [?c :commit/changes ?ch2]
+                                 [(!= ?ch1 ?ch2)]
+                                 [?ch2 :change/path ?f2]
+                                 [?f2 :file/path ?p2]
+                          ;[(.endsWith ?p2 ".rb")]
+                                 ]
+                               db r file-path))))
